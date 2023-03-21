63929e156bfb0a4e6e5ccb57a278336dbddb3ad4
Evgeny Efimov <evgenofonov@gmail.com>
2023-03-22 00:04:27 +0300

    Add Greet.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.cli();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+        int gameChoice = scanner.nextInt();
+        switch (gameChoice) {
+            case 0: scanner.close();
+                break;
+            case 1: Greet.greet();
+                break;
+        }
+
     }
 }
