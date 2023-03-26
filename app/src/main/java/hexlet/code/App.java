177ae4d6e967bb3eb81610bc9afc0f7166a5882e
Evgeny Efimov <evgenofonov@gmail.com>
2023-03-26 22:18:40 +0300

    Fix App.java add even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,14 +8,18 @@ public class App {
 
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
+        System.out.println("2 - Even");
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
         int gameChoice = scanner.nextInt();
         switch (gameChoice) {
-            case 0: scanner.close();
-                break;
-            case 1: Greet.greet();
-                break;
+            case 0 -> scanner.close();
+            case 1 -> {
+                Greet.greet();
+                scanner.close();
+            }
+            case 2 -> Even.even();
+            default -> scanner.close();
         }
 
     }
