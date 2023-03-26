e6c22fac52dc270b3ad6880082eaa2929a980f20
Evgeny Efimov <evgenofonov@gmail.com>
2023-03-27 00:49:20 +0300

    Add Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+import java.util.Scanner;
+public class Engine {
+    final public static int minNumber = 0;
+    final public static int maxNumber = 100;
+    final static int numberOfRounds = 3;
+    public static String gameRules = "";
+    public static void gameEngine(String[] questions, String[] expectedAnswers) {
+        Greet.greet();
+        System.out.println(gameRules);
+        for (int i = 0; i < numberOfRounds; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Question: " + questions[i]);
+            System.out.println("Your answer: ");
+            String userAnswer = scanner.next();
+            if (expectedAnswers[i].equalsIgnoreCase(userAnswer)) {
+                System.out.println("Correct!");
+                if (i == (numberOfRounds - 1)) {
+                    System.out.println("Congratulations, " + User.getUserName() + "!");
+                    scanner.close();
+                }
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + expectedAnswers[i] + "'.");
+                System.out.println("Let's try again " + User.getUserName() + "!");
+                scanner.close();
+                i = numberOfRounds;
+            }
+        }
+    }
+
+}
