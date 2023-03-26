7abb1ba05de06b96d8443347e81ce75ac78edd58
Evgeny Efimov <evgenofonov@gmail.com>
2023-03-27 01:16:14 +0300

    Fix Engine.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,47 +1,20 @@
 package hexlet.code;
 
-import java.util.Scanner;
 public class Even {
     public static void even() {
-        Scanner scanner = new Scanner(System.in);
-
-        final int minNumber = 0;
-        final int maxNumber = 1000;
-        Greet.greet();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        final int numberOfRounds = 3;
-        for (int i = 0; i < numberOfRounds; i++) {
-            int randomNumber = minNumber + (int) (Math.random() * maxNumber);
-            System.out.println("Question: " + randomNumber);
-            System.out.println("Your answer: ");
-            String userAnswer = scanner.next();
+        String[] questions = new String[Engine.numberOfRounds];
+        String[] expectedAnswers = new String[Engine.numberOfRounds];
+        for (int i = 0; i < Engine.numberOfRounds; i++) {
+            int randomNumber = Engine.minNumber + (int) (Math.random() * Engine.maxNumber);
             boolean evenNumber = ((randomNumber % 2) == 0);
-            boolean oddNumber = ((randomNumber % 2) == 1);
-            boolean answerYes = userAnswer.equalsIgnoreCase("yes");
-            boolean answerNo = userAnswer.equalsIgnoreCase("no");
-            if ((evenNumber && answerYes) || oddNumber && answerNo) {
-                System.out.println("Correct!");
-                if (i == (numberOfRounds - 1)) {
-                    System.out.println("Congratulations, " + User.getUserName() + "!");
-                    scanner.close();
-                }
+            questions[i] = Integer.toString(randomNumber);
+            if (evenNumber) {
+                expectedAnswers[i] = "yes";
             } else {
-                if (answerYes) {
-                    System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                    System.out.println("Let's try again " + User.getUserName() + "!");
-                    scanner.close();
-                    i = numberOfRounds;
-                } else if (answerNo) {
-                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                    System.out.println("Let's try again " + User.getUserName() + "!");
-                    scanner.close();
-                    i = numberOfRounds;
-                } else {
-                    System.out.println("Let's try again " + User.getUserName() + "!");
-                    scanner.close();
-                    i = numberOfRounds;
-                }
+                expectedAnswers[i] = "no";
             }
         }
+        Engine.gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        Engine.gameEngine(questions, expectedAnswers);
     }
 }
